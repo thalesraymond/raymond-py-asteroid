358b1e9557132ef1c8b76775b44d723c0b3c4bbc
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-02-28 14:37:53 -0300

    Update .gitignore to include game_state.jsonl and refactor main.py for game initialization and event handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 
@@ -7,6 +8,25 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill("black")
+
+    while True:
+        log_state()
+        dt = game_clock.tick(60) / 1000
+        print(dt)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
 
 if __name__ == "__main__":
     main()
